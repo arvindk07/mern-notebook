813259c240e5bade845d15f5a9f56427f8be0378
arvindk07 <info.arvind07@gmail.com>
2023-05-15 10:47:11 +0530

    initialize project

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import notes from "./data/notes";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server started on  ${PORT}`));
